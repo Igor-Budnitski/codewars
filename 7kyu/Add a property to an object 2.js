e237cb338b70842eb17317693210eb58e718a6f8
Igor Budnitski <igor.budnitski@gmail.com>
2025-09-27 19:55:11 +0300

    add a property to an object.

--- 7kyu/Add a property to an object 2.js
+++ 7kyu/Add a property to an object 2.js
@@ -7,3 +7,46 @@ function addProperty(obj, prop, value) {
     if (obj.hasOwnProperty(prop)) throw "Error: Property already present";
     obj[prop] = value;
 }
+
+const user = {
+    name: 'Igor',
+    age: 23,
+}
+
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+
+console.log(user.hasOwnProperty("fds"));
+
+console.log(user.toLocaleString());
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const grouped = Object.groupBy(numbers, num =>
+    num % 2 === 0 ? "even" : "odd"
+);
+
+console.log(grouped);
+// {
+//   odd: [1, 3, 5],
+//   even: [2, 4, 6]
+// }
+
+
+const people = [
+    { name: "Alice", age: 23 },
+    { name: "Bob", age: 30 },
+    { name: "Charlie", age: 23 },
+];
+
+const byAge = Object.groupBy(people, person => person.age);
+
+console.log(byAge);
+/*
+{
+  23: [ { name: "Alice", age: 23 }, { name: "Charlie", age: 23 } ],
+  30: [ { name: "Bob", age: 30 } ]
+}
+*/
